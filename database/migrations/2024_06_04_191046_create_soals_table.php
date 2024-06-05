55e5450f6a03af96aeee4c6d4bb572b53c1a0d89
MazoDreX <danielprasetyo053@gmail.com>
2024-06-05 16:05:34 +0700

    Membuat tabel soal pada migrations 2024_06_04_191046_create_soals_table

--- database/migrations/2024_06_04_191046_create_soals_table.php
+++ database/migrations/2024_06_04_191046_create_soals_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('soals', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('subbab_id')->constrained()->cascadeOnDelete();
+            $table->text('soal'); // Kolom untuk menyimpan soal
+            $table->text('jawaban'); // Kolom untuk menyimpan jawaban yang benar
+            $table->text('option_a'); // Kolom untuk menyimpan opsi A
+            $table->text('option_b'); // Kolom untuk menyimpan opsi B
+            $table->text('option_c'); // Kolom untuk menyimpan opsi C
+            $table->text('option_d'); // Kolom untuk menyimpan opsi D
             $table->timestamps();
         });
     }
