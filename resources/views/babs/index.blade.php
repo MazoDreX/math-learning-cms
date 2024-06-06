4d2f0a39d7bebb2eafaaebe4fab1212cc0e342de
MazoDreX <danielprasetyo053@gmail.com>
2024-06-07 02:48:54 +0700

    Major, membuat tampilan halaman utama beserta fiturnya seperti Soal dan MathJax

---
+++ resources/views/babs/index.blade.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Belajar Matematika - Daftar Bab</title>
+  <link rel="icon" type="image/x-icon" href="{{ asset('/images/math-icon.png') }}">
+  @vite('resources/css/app.css')
+</head>
+
+<body>
+<div class="flex flex-col min-h-screen bg-gray-100 dark:bg-gray-900">
+    <header class="bg-white dark:bg-gray-800 shadow-md px-4 py-5 flex justify-between items-center">
+    <a href="/" class="text-xl font-bold text-gray-800 dark:text-gray-100">Belajar Matematika</a>
+      <nav class="space-x-4">
+        <button id="toggleDarkMode" class="text-gray-600 hover:text-gray-800 dark:text-gray-200 dark:hover:text-gray-100">
+          <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 0 1-9 9m-9-9a9 9 0 0 0 9-9" />
+          </svg>
+        </button>
+        <a href="/admin" class="text-gray-600 hover:text-gray-800 dark:text-gray-200 dark:hover:text-gray-100">Admin</a>
+      </nav>
+    </header>
+    
+    <main class="flex-grow px-4 py-8">
+      <h1 class="text-3xl font-bold text-center mb-8 dark:text-gray-100">Daftar Bab</h1>
+      <div class="grid grid-cols-3 gap-4">
+        <div class="bg-white shadow-md rounded-md px-4 py-5 hover:shadow-lg dark:bg-gray-700">
+          <h2 class="text-xl font-bold dark:text-white">Kelas 10</h2>
+          @foreach ($babs as $bab)
+          @if ($bab->kelas == 10)
+          <a href="{{url('/bab/' . $bab->slug)}}" class="text-xl font-small text-gray-800 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400">
+            {{$bab->judul}}
+          </a>
+          <p class="text-gray-600 dark:text-gray-400 mt-2"></p>
+          @endif
+          @endforeach
+        </div>
+
+        <div class="bg-white shadow-md rounded-md px-4 py-5 hover:shadow-lg dark:bg-gray-700">
+          <h2 class="text-xl font-bold dark:text-white">Kelas 11</h2>
+          @foreach ($babs as $bab)
+          @if ($bab->kelas == 11)
+          <a href="{{url('/bab/' . $bab->slug)}}" class="text-xl font-medium text-gray-800 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400">
+            {{$bab->judul}}
+          </a>
+          <p class="text-gray-600 dark:text-gray-400 mt-2">Kelas {{$bab->kelas}}</p>
+          @endif
+          @endforeach
+        </div>
+
+        <div class="bg-white shadow-md rounded-md px-4 py-5 hover:shadow-lg dark:bg-gray-700">
+          <h2 class="text-xl font-bold dark:text-white">Kelas 12</h2>
+          @foreach ($babs as $bab)
+          @if ($bab->kelas == 12)
+          <a href="{{url('/bab/' . $bab->slug)}}" class="text-xl font-medium text-gray-800 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400">
+            {{$bab->judul}}
+          </a>
+          <p class="text-gray-600 dark:text-gray-400 mt-2">Kelas {{$bab->kelas}}</p>
+          @endif
+          @endforeach
+        </div>
+      </div>
+    </main>
+  </div>
+  <!-- <script>
+    const darkMode = document.getElementById('toggleDarkMode');
+    darkMode.addEventListener('click', () => {
+      document.body.classList.toggle('dark');
+    });
+
+  </script> -->
+</body>
+
+</html>
---
+++ resources/views/babs/show.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bab - {{ $bab->judul }}</title>
+    <link rel="icon" type="image/x-icon" href="{{ asset('/images/math-icon.png') }}">
+    @vite('resources/css/app.css')
+</head>
+<body>
+</nav>
+  <div class="flex flex-col min-h-screen bg-gray-100 dark:bg-gray-900">
+    <header class="bg-white dark:bg-gray-800 shadow-md px-4 py-5 flex justify-between items-center">
+    <a href="/" class="text-xl font-bold text-gray-800 dark:text-gray-100">Belajar Matematika</a>
+      <nav class="space-x-4">
+        <button id="toggleDarkMode" class="text-gray-600 hover:text-gray-800 dark:text-gray-200 dark:hover:text-gray-100">
+          <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 0 1-9 9m-9-9a9 9 0 0 0 9-9" />
+          </svg>
+        </button>
+        <a href="/admin" class="text-gray-600 hover:text-gray-800 dark:text-gray-200 dark:hover:text-gray-100">Admin</a>
+      </nav>
+    </header>
+
+
+    <h1 class="text-3xl font-bold text-center mb-8 my-6 dark:text-gray-100">{{ $bab->judul }}</h1>
+    @if($subbabs && count($subbabs) > 0)
+    <div class="grid grid-cols-2 gap-4 place-content-center h-25">
+        @foreach ($subbabs as $subbab)
+            <div class="bg-white shadow-md rounded-lg p-4 place-items-center">
+                <a class="text-center" href="{{ url('/subbab/' . $subbab->slug) }}" >
+                    <div class="flex flex-col">
+                        <h3 class="font-bold">{{ $subbab->subbabJudul }}</h3>
+                    </div>
+                </a>
+            </div>
+        @endforeach
+    </div>
+@else
+    <p class="mt-6 text-gray-600">Tidak ada subbab yang tersedia.</p>
+@endif
+</body>
+</html>
