57771db780a6d4cdcf35ab47a1cd551f8459f2f5
MazoDreX <danielprasetyo053@gmail.com>
2024-06-06 23:30:58 +0700

    Membuat routing sesuai dengan controller masing-masing

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\BabController;
+use App\Http\Controllers\SubbabController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [BabController::class, 'index']);
+Route::get('/bab/{slug}', [BabController::class, 'show']);
+Route::get('/subbab/{slug}', [SubbabController::class, 'show']);
