084983056ebc94f1308aad5fb6fbf6f6d8b5b530
MazoDreX <danielprasetyo053@gmail.com>
2024-06-14 16:10:25 +0700

    added selanjutnya, kembali, and creator

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://belajar-matematika-sma.vercel.app/'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     /*
     |--------------------------------------------------------------------------
--- resources/views/subbabs/show.blade.php
+++ resources/views/subbabs/show.blade.php
@@ -1,11 +1,50 @@
 <x-layout>
 <title>Belajar Matematika - {{ $subbab->subbabJudul }}</title>
+
     <x-navbar/>
-<div class="bg-gray-100 dark:bg-neutral-900">
-    <div class="container mx-auto p-6 bg-gray-100 dark:bg-neutral-900">
-        <h1 class="text-3xl font-bold text-center mb-6 dark:text-neutral-100">{{ $subbab->subbabJudul }}</h1>
+    
+<div class="bg-neutral-100 dark:bg-neutral-900">
+    <div class="container mx-auto p-6 bg-neutral-100 dark:bg-neutral-900">
+        
+        <div class="flex justify-between  items-center mb-6">
+            <a href="{{ url('/bab/' . $subbab->bab->slug) }}" class="text-neutral-600 hover:text-neutral-800 dark:text-neutral-400 dark:hover:text-neutral-200">Kembali</a>
+            
+            @if ($nextSubbab)
+                <a href="{{ url('/subbab/' . $nextSubbab->slug) }}" class="text-neutral-600 hover:text-neutral-800 dark:text-neutral-400 dark:hover:text-neutral-200">Selanjutnya</a>
+            @else
+                <p class="text-neutral-600 dark:text-neutral-400">Tidak ada subbab selanjutnya</p> 
+            @endif
+        </div>
+        
+        
+
         <div class="content bg-white shadow-md rounded-lg p-6 mb-6 dark:text-white dark:bg-neutral-700">
+        <h1 class="text-3xl font-bold text-center dark:text-neutral-100 mb-6">{{ $subbab->subbabJudul }}</h1>
+            <div class="flex justify-between items-center mb-6">
+                @if ($tags)
+                    <div class="flex flex-wrap mb-4 items-center">
+                        @foreach ($tags as $tag)
+                            <span class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">{{ $tag }}</span>
+                        @endforeach
+                    </div>
+                @endif
+            </div>
+
+            <!-- BAGIAN ISI SUBBAB -->
             {!! $subbab->subbabIsi !!}
+
+
+                                    <!-- BAGIAN PEMBUAT DAN TANGGAL PEMBUATAN-->
+            <div class="flex justify-between items-center space-x-4 bg-white dark:bg-neutral-700 p-4 rounded-lg mt-8">
+                <div></div>
+                <div class="flex justify-between items-center space-x-4 bg-white dark:bg-neutral-700 p-4 rounded-lg shadow-md">
+                    <p class="text-lg italic font-medium text-neutral-900 dark:text-neutral-100">{{ $subbab->creator }}</p>
+                    <p class="text-neutral-600 dark:text-neutral-400">Dibuat {{ $subbab->created_at->translatedFormat('d F Y') }}</p>
+                    <p class="text-neutral-600 dark:text-neutral-400">{{ $subbab->created_at->diffForHumans() }}</p>
+                </div>
+            </div>
+
+
         </div>
         <h2 class="text-2xl font-semibold mb-4 text-center dark:text-neutral-100">Soal Latihan</h2>
         @if ($soals && count($soals) > 0)
@@ -79,4 +118,5 @@
     }
 
     </script>
+
 </x-layout>
