6bcde799c13f3dd08d9b1630007fc453212e6034
Samuel Yahya <samuelyahya25@gmail.com>
2024-07-02 09:52:42 +0700

    feat(modal): add overlay notification

--- public/js/subbabScript.js
+++ public/js/subbabScript.js
@@ -1,93 +1,91 @@
 MathJax = {
     tex: {
         inlineMath: [
-            ['$', '$'],
-            ['\\(', '\\)']
-        ]
-    }
+            ["$", "$"],
+            ["\\(", "\\)"],
+        ],
+    },
 };
 
+document.addEventListener("DOMContentLoaded", function () {
+    const totalSoal = document.querySelector('meta[name="jumlahSoal"]').content;
+    let jawabanBenar = 0;
+    let jawabanSalah = 0;
+    let soalTerjawab = 0;
 
-document.addEventListener("DOMContentLoaded", function() {
+    window.checkAnswer = function (soalId) {
+        console.log("TOTAL SOAL = ", totalSoal);
+        const form = document.getElementById(`form-${soalId}`);
+        const formData = new FormData(form);
+        const selectedOption = formData.get("option");
+        const correctAnswer = form.querySelector(
+            'input[name="correct_answer"]'
+        ).value;
+        const result = document.getElementById(`result-${soalId}`);
+        const right_answer = result.getAttribute("data-Right_Answer");
 
-
-const totalSoal = document.querySelector('meta[name="jumlahSoal"]').content;
-let jawabanBenar = 0;
-let jawabanSalah = 0;
-let soalTerjawab = 0;
-
-window.checkAnswer = function(soalId) {
-    console.log("TOTAL SOAL = " ,totalSoal);
-    const form = document.getElementById(`form-${soalId}`);
-    const formData = new FormData(form);
-    const selectedOption = formData.get('option');
-    const correctAnswer = form.querySelector('input[name="correct_answer"]').value;
-    const result = document.getElementById(`result-${soalId}`);
-    const thumbUp_emoji = result.getAttribute('data-thumbUp_emoji');
-
-    const resultDiv = document.getElementById(`result-${soalId}`);
-    const resultDivText = document.getElementById(`result-${soalId}-text`);
-    if (selectedOption) {
-        if (selectedOption === correctAnswer) {
-            resultDiv.innerHTML = `
-            <dotlottie-player  src="${thumbUp_emoji}" background="transparent" speed="1" style="width: 100px; height: auto;" loop autoplay></dotlottie-player>
+        const resultDiv = document.getElementById(`result-${soalId}`);
+        const resultDivText = document.getElementById(`result-${soalId}-text`);
+        if (selectedOption) {
+            if (selectedOption === correctAnswer) {
+                resultDiv.innerHTML = `
+            <dotlottie-player  src="${right_answer}" background="transparent" speed="1" style="width: 45px; height: auto;" null autoplay></dotlottie-player>
             `;
-            resultDivText.innerHTML = `
+                resultDivText.innerHTML = `
             <p class="correct relative text-green-500">Jawaban Anda benar!</p>`;
-            jawabanBenar++;
-            console.log("JAWABAN BENAR = " ,jawabanBenar);
-        } else {
-            resultDiv.innerHTML = `
-            <dotlottie-player src="https://lottie.host/922162ff-92fc-4428-9034-680ebff8347f/M0GBi9Zx4k.json" background="transparent" speed="0.75" style="width: 100px; height: auto;" loop autoplay></dotlottie-player>
+                jawabanBenar++;
+                console.log("JAWABAN BENAR = ", jawabanBenar);
+            } else {
+                resultDiv.innerHTML = `
+            <dotlottie-player src="https://lottie.host/824332f6-45ee-4c10-bbfe-4cafa0613444/3a9xcCoeJ6.json" background="transparent" speed="1" style="width: 45px; height: auto;" null autoplay></dotlottie-player>
             `;
-            resultDivText.innerHTML = `<p class="incorrect relative text-red-500">Jawaban Anda salah. Jawaban yang benar adalah ${correctAnswer.toUpperCase()}.</p>`;
-            jawabanSalah++;
-            console.log("JAWABAN SALAH = " ,jawabanSalah);
+                resultDivText.innerHTML = `<p class="incorrect relative text-red-500">Jawaban Anda salah. Jawaban yang benar adalah ${correctAnswer.toUpperCase()}.</p>`;
+                jawabanSalah++;
+                console.log("JAWABAN SALAH = ", jawabanSalah);
+            }
+            soalTerjawab++;
+            console.log("SOAL TERJAWAB = ", soalTerjawab);
+        } else {
+            resultDiv.innerHTML = `<p class="incorrect text-red-500">Anda belum memilih jawaban.</p>`;
         }
-        soalTerjawab++;
-        console.log( "SOAL TERJAWAB = " ,soalTerjawab);
-    } else {
-        resultDiv.innerHTML = `<p class="incorrect text-red-500">Anda belum memilih jawaban.</p>`;
-    }
 
+        // Menonaktifkan semua input radio setelah submit
+        const radioButtons = form.querySelectorAll('input[type="radio"]');
+        if (selectedOption) {
+            radioButtons.forEach((button) => {
+                button.disabled = true;
+            });
+        }
 
-    // Menonaktifkan semua input radio setelah submit
-    const radioButtons = form.querySelectorAll('input[type="radio"]');
-    if (selectedOption) {
-        radioButtons.forEach(button => {
-            button.disabled = true;
-        });
-    }
+        // Menonaktifkan tombol submit setelah submit
+        const submitButton = form.querySelector('button[type="button"]');
+        if (selectedOption) {
+            submitButton.disabled = true;
+        }
 
-    // Menonaktifkan tombol submit setelah submit
-    const submitButton = form.querySelector('button[type="button"]');
-    if (selectedOption) {
-        submitButton.disabled = true;
-    }
+        if (soalTerjawab == totalSoal) {
+            console.log("SELESAI, KODE MENCAPAI BLOK AKHIR");
+            showScore();
+        }
+    };
 
-    if (soalTerjawab == totalSoal) {
-        console.log("SELESAI, KODE MENCAPAI BLOK AKHIR");
-        showScore();
-    }
-    
-}
+    function showScore() {
+        const scoreDiv = document.getElementById("score");
+        const scorePercentage = (jawabanBenar / totalSoal) * 100;
+        const scoreText = document.querySelectorAll(".score-text");
+        const modal = document.getElementById("modal");
+        const body = document.querySelector("body");
+        const lottieURL_SAD = modal.getAttribute("data-sad_emoji");
+        const lottieURL_HAPPY = modal.getAttribute("data-happy_emoji");
+        const lottieURL_STRONG = modal.getAttribute("data-strong_emoji");
 
-function showScore() {
-    const scoreDiv = document.getElementById('score');
-    const scorePercentage = (jawabanBenar / totalSoal) * 100;
-    const scoreText = document.querySelectorAll('.score-text');
-    const modal = document.getElementById('modal');
-    const body = document.querySelector('body');
-    const lottieURL_SAD = modal.getAttribute('data-sad_emoji');
-    const lottieURL_HAPPY = modal.getAttribute('data-happy_emoji');
-    const lottieURL_STRONG = modal.getAttribute('data-strong_emoji');
-    
-    if (jawabanBenar == totalSoal && scoreText.length > 0 ) {
-        body.style.overflow = 'hidden';
-        scoreText.forEach(element => {
-            element.classList.add('text-green-500');
-        });
-        modal.innerHTML = `
+        if (jawabanBenar == totalSoal && scoreText.length > 0) {
+            body.style.overflow = "hidden";
+            scoreText.forEach((element) => {
+                element.classList.add("text-green-500");
+            });
+            modal.innerHTML = `
+            <div id="overlay" class="overlay"></div>
             <div  class="modal-container modal-center">
                 <div class="lottie-container relative">
                     <dotlottie-player class="icon"  src="${lottieURL_HAPPY}" background="transparent" speed="1" style="width: 200px; height: auto;" loop autoplay></dotlottie-player>
@@ -101,31 +99,39 @@ function showScore() {
                     Nilai anda sempurna
                     <div class="flex-popup place-content-center">
                         <div class="">Skor Anda : </div>
-                        <div id="score" class="ml-2 font-semibold text-green-500">${jawabanBenar}/${totalSoal} (${scorePercentage.toFixed(2)})</div>
+                        <div id="score" class="ml-2 font-semibold text-green-500">${jawabanBenar}/${totalSoal} (${scorePercentage.toFixed(
+                2
+            )})</div>
                     </div>
                 </div>
                 <div class="dismiss-btn">
                     <button id="dismiss-popup-btn">OK</button>
                 </div>
             </div>
+
         `;
-        modal.classList.add("active");
-        const dismissBtn = document.getElementById('dismiss-popup-btn');
-        
-        dismissBtn.addEventListener('click', function(){
-            modal.classList.remove("active");
-            modal.classList.add('exit');
-            document.documentElement.classList.remove('modal-active');
-            document.body.classList.remove('modal-active');
-            modal.addEventListener('transitioned', function(){
-                modal.innerHTML = ``;
-                modal.classList.remove('exit');
-                
-            }, { once: true });
-            body.style.overflow = 'auto';
-        });
-    } else if( scorePercentage >= 50 && scorePercentage < 100)  {
-        modal.innerHTML = `
+            modal.classList.add("active");
+            const dismissBtn = document.getElementById("dismiss-popup-btn");
+
+            dismissBtn.addEventListener("click", function () {
+                modal.classList.remove("active");
+                modal.classList.add("exit");
+                document.documentElement.classList.remove("modal-active");
+                document.body.classList.remove("modal-active");
+                modal.addEventListener(
+                    "transitioned",
+                    function () {
+                        modal.innerHTML = ``;
+                        modal.classList.remove("exit");
+                    },
+                    { once: true }
+                );
+                body.style.overflow = "auto";
+                document.getElementById("overlay").style.display = "none";
+            });
+        } else if (scorePercentage >= 50 && scorePercentage < 100) {
+            modal.innerHTML = `
+            <div id="overlay" class="overlay"></div>
             <div  class="modal-container modal-center">
                 <div class="lottie-container relative">
                     <dotlottie-player class="icon"  src="${lottieURL_STRONG}" background="transparent" speed="1" style="width: 200px; height: auto;" loop autoplay></dotlottie-player>
@@ -139,7 +145,9 @@ function showScore() {
                     Anda masih bisa berkembang dengan belajar lebih banyak
                     <div class="flex-popup place-content-center">
                         <div class="">Skor Anda : </div>
-                        <div id="score" class="ml-2 font-semibold ">${jawabanBenar}/${totalSoal} (${scorePercentage.toFixed(2)})</div>
+                        <div id="score" class="ml-2 font-semibold ">${jawabanBenar}/${totalSoal} (${scorePercentage.toFixed(
+                2
+            )})</div>   
                     </div>
                 </div>
                 <div class="dismiss-btn">
@@ -147,31 +155,38 @@ function showScore() {
                 </div>
             </div>
         `;
-        modal.classList.add("active");
-        const dismissBtn = document.getElementById('dismiss-popup-btn');
-        
-        dismissBtn.addEventListener('click', function(){
-            modal.classList.remove("active");
-            modal.classList.add('exit');
-            document.documentElement.classList.remove('modal-active');
-            document.body.classList.remove('modal-active');
-            modal.addEventListener('transitioned', function(){
-                modal.innerHTML = ``;
-                modal.classList.remove('exit');
-                
-            }, { once: true });
-            body.style.overflow = 'auto';
-        });
-    } else if( scorePercentage == 0 || scorePercentage < 50 && scoreText.length > 0 ) {
+            modal.classList.add("active");
+            const dismissBtn = document.getElementById("dismiss-popup-btn");
 
-        scoreText.forEach(element => {
-            element.classList.add('text-red-500')
-        });
-        body.style.overflow = 'hidden';
-        modal.innerHTML = `
+            dismissBtn.addEventListener("click", function () {
+                modal.classList.remove("active");
+                modal.classList.add("exit");
+                document.documentElement.classList.remove("modal-active");
+                document.body.classList.remove("modal-active");
+                modal.addEventListener(
+                    "transitioned",
+                    function () {
+                        modal.innerHTML = ``;
+                        modal.classList.remove("exit");
+                    },
+                    { once: true }
+                );
+                body.style.overflow = "auto";
+                document.getElementById("overlay").style.display = "none";
+            });
+        } else if (
+            scorePercentage == 0 ||
+            (scorePercentage < 50 && scoreText.length > 0)
+        ) {
+            scoreText.forEach((element) => {
+                element.classList.add("text-red-500");
+            });
+            body.style.overflow = "hidden";
+            modal.innerHTML = `
+            <div id="overlay" class="overlay"></div>
             <div  class="modal-container modal-center">
                 <div class="lottie-container relative">
-                   <dotlottie-player src="${lottieURL_SAD}" background="transparent" speed="1"   loop autoplay></dotlottie-player>
+                <dotlottie-player src="${lottieURL_SAD}" background="transparent" speed="1"   loop autoplay></dotlottie-player>
                 </div>
                 <div class="title">
                     Yah...
@@ -181,7 +196,9 @@ function showScore() {
                     Belajar lebih semangat lagi....
                     <div class="flex-popup place-content-center">
                         <div class="">Skor Anda : </div>
-                        <div id="score" class="ml-2 font-semibold text-red-500">${jawabanBenar}/${totalSoal} (${scorePercentage.toFixed(2)})</div>
+                        <div id="score" class="ml-2 font-semibold text-red-500">${jawabanBenar}/${totalSoal} (${scorePercentage.toFixed(
+                2
+            )})</div>
                     </div>
                 </div>
                 <div class="dismiss-btn">
@@ -189,26 +206,29 @@ function showScore() {
                 </div>
             </div>
         `;
-        modal.classList.add("active");
-        const dismissBtn = document.getElementById('dismiss-popup-btn');
-        
-        dismissBtn.addEventListener('click', function(){
-            modal.classList.remove("active");
-            modal.classList.add('exit');
-            document.documentElement.classList.remove('modal-active');
-            document.body.classList.remove('modal-active');
-            modal.addEventListener('transitioned', function(){
-                modal.innerHTML = ``;
-                modal.classList.remove('exit');
-                
-            }, { once: true });
-            body.style.overflow = 'auto';
-        });
+            modal.classList.add("active");
+            const dismissBtn = document.getElementById("dismiss-popup-btn");
+
+            dismissBtn.addEventListener("click", function () {
+                modal.classList.remove("active");
+                modal.classList.add("exit");
+                document.documentElement.classList.remove("modal-active");
+                document.body.classList.remove("modal-active");
+                modal.addEventListener(
+                    "transitioned",
+                    function () {
+                        modal.innerHTML = ``;
+                        modal.classList.remove("exit");
+                    },
+                    { once: true }
+                );
+                body.style.overflow = "auto";
+                document.getElementById("overlay").style.display = "none";
+            });
+        }
+
+        scoreDiv.innerHTML = `${jawabanBenar}/${totalSoal} (${scorePercentage.toFixed(
+            2
+        )})`;
     }
-
-    scoreDiv.innerHTML = `${jawabanBenar}/${totalSoal} (${scorePercentage.toFixed(2)})`;
-
-}
-
 });
-
--- resources/views/subbabs/show.blade.php
+++ resources/views/subbabs/show.blade.php
@@ -1,10 +1,10 @@
 <x-layout>
     <title>Belajar Matematika - {{ $subbab->subbabJudul }}</title>
     <script id="MathJax-script" async src="{{asset('js/mathjax/tex-chtml.js')}}"></script>
-    <script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script> 
+    <script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script>
     <script src="{{ asset('js/subbabScript.js') }} "></script>
 
-<!-- SANGAT TERPAKSA MENGGUNAKAN CDN KARENA HARUS BUILD SETIAP KALI MUNCUL DARI JAVASCRIPT :'(-->
+    <!-- SANGAT TERPAKSA MENGGUNAKAN CDN KARENA HARUS BUILD SETIAP KALI MUNCUL DARI JAVASCRIPT :'(-->
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
         tailwind.config = {
@@ -21,15 +21,12 @@
         }
     </script>
 
-     <link rel="stylesheet" href="{{ asset('css/filament/popup.css') }} "></link>
+    <link rel="stylesheet" href="{{ asset('css/filament/popup.css') }} ">
+    </link>
     <meta name="jumlahSoal" content="{{ $subbab->soals->count() }}">
     <meta name="sad_emoji" content="{{ url('lottie/sad_emoji.lottie') }}">
 
-    <div type="hidden" id="modal" 
-        data-sad_emoji="{{ url('lottie/sad_emoji.lottie') }}"
-        data-happy_emoji="{{ url('lottie/happy_emoji.lottie') }}"
-        data-strong_emoji="{{ url('lottie/strong_emoji.lottie') }}"    
-    > </div>
+    <div type="hidden" id="modal" data-sad_emoji="{{ url('lottie/sad_emoji.lottie') }}" data-happy_emoji="{{ url('lottie/happy_emoji.lottie') }}" data-strong_emoji="{{ url('lottie/strong_emoji.lottie') }}"> </div>
 
     <x-navbar />
     <div id="main" class=" min-h-screen bg-gradient-to-b from-neutral-200 to-neutral-300 dark:from-neutral-950 dark:to-indigo-950 py-20">
@@ -60,10 +57,9 @@
                 </div>
 
                 <!-- BAGIAN ISI SUBBAB, BISA DIISI DENGAN TAILWINDCSS YANG BELUM DIBUILD -->
-                 
-                {!! $subbab->subbabIsi !!}
-                
-
+                <div class="">
+                    {!! $subbab->subbabIsi !!}
+                </div>
 
                 <!-- BAGIAN PEMBUAT DAN TANGGAL PEMBUATAN-->
                 <div class="flex justify-between items-center space-x-4 bg-white dark:bg-neutral-700 p-4 rounded-lg mt-8">
@@ -76,54 +72,54 @@
                 </div>
 
 
-                
+
             </div>
             <h2 class="text-2xl font-semibold mb-4 text-center dark:text-neutral-100">Soal Latihan</h2>
             <!-- BAGIAN SKOR -->
-            <div class="flex ">
-                    <div class="flex space-x-4 bg-white dark:bg-neutral-700 p-4 rounded-lg shadow-md">
-                        <div class="font-semibold dark:text-neutral-50">Skor Anda : </div>
-                        <div id="score" class="score-text ml-2 font-semibold dark:text-neutral-50">0</div>
-                    </div>
+            <div class="flex mb-5">
+                <div class="flex space-x-4 bg-white dark:bg-neutral-700 p-4 rounded-lg shadow-md">
+                    <div class="font-semibold dark:text-neutral-50">Skor Anda : </div>
+                    <div id="score" class="score-text ml-2 font-semibold dark:text-neutral-50">0</div>
+                </div>
             </div>
             @if ($soals && count($soals) > 0)
             <div class="space-y-4 ">
                 @foreach ($soals as $soal)
-                <div class="soal bg-white shadow-md rounded-lg p-6 dark:text-white dark:bg-neutral-700">
-                    <p class="mb-4">{!! $soal->soal !!}</p>
-                    <form id="form-{{ $soal->id }}">
-                        <ul class="space-y-2">
-                            <li class="flex items-center">
-                                <input type="radio" name="option" value="a" id="option_a_{{ $soal->id }}">
-                                <label for="option_a_{{ $soal->id }}" class="ml-2">{!! $soal->option_a !!}</label>
-                            </li>
-                            <li class="flex items-center">
-                                <input type="radio" name="option" value="b" id="option_b_{{ $soal->id }}">
-                                <label for="option_b_{{ $soal->id }}" class="ml-2">{!! $soal->option_b !!}</label>
-                            </li>
-                            <li class="flex items-center">
-                                <input type="radio" name="option" value="c" id="option_c_{{ $soal->id }}">
-                                <label for="option_c_{{ $soal->id }}" class="ml-2">{!! $soal->option_c !!}</label>
-                            </li>
-                            <li class="flex items-center">
-                                <input type="radio" name="option" value="d" id="option_d_{{ $soal->id }}">
-                                <label for="option_d_{{ $soal->id }}" class="ml-2">{!! $soal->option_d !!}</label>
-                            </li>
-                        </ul>
-                        <div class="flex">
+                <div class="soal bg-white flex shadow-md rounded-lg p-6 dark:text-white dark:bg-neutral-700 justify-between">
+                    <div>
+                        <p class="mb-4">{!! $soal->soal !!}</p>
+                        <form id="form-{{ $soal->id }}">
+                            <ul class="space-y-2">
+                                <li class="flex items-center">
+                                    <input type="radio" name="option" value="a" id="option_a_{{ $soal->id }}">
+                                    <label for="option_a_{{ $soal->id }}" class="ml-2">{!! $soal->option_a !!}</label>
+                                </li>
+                                <li class="flex items-center">
+                                    <input type="radio" name="option" value="b" id="option_b_{{ $soal->id }}">
+                                    <label for="option_b_{{ $soal->id }}" class="ml-2">{!! $soal->option_b !!}</label>
+                                </li>
+                                <li class="flex items-center">
+                                    <input type="radio" name="option" value="c" id="option_c_{{ $soal->id }}">
+                                    <label for="option_c_{{ $soal->id }}" class="ml-2">{!! $soal->option_c !!}</label>
+                                </li>
+                                <li class="flex items-center">
+                                    <input type="radio" name="option" value="d" id="option_d_{{ $soal->id }}">
+                                    <label for="option_d_{{ $soal->id }}" class="ml-2">{!! $soal->option_d !!}</label>
+                                </li>
+                            </ul>
                             <input type="hidden" name="correct_answer" value="{{ $soal->jawaban }}">
-                            <div class="relative mt-10">
-                                <button type="button" onclick="checkAnswer({{ $soal->id }});" class="mt-4 bg-blue-500 text-white px-4 py-2 rounded">Submit</button>
+                            <div class="relative my-5">
+                                <button type="button" onclick="checkAnswer({{ $soal->id }});" class="bg-blue-500 text-white px-4 py-2 rounded">Submit</button>
                             </div>
-                            <div id="result-{{ $soal->id }}" 
-                                data-thumbUp_emoji="{{ url('lottie/thumbUp_emoji.lottie') }} "
-                            class="mt-2 p-4 rounded"></div>
-                        </div>
-                    </form>
+                        </form>
                         <div id="result-{{ $soal->id }}-text"></div>
+                    </div>
+                    <div class="self-start">
+                        <div id="result-{{ $soal->id }}" data-Right_Answer="{{ url('lottie/Right_Answer.lottie') }} " class="rounded"></div>
+                    </div>
                 </div>
                 @endforeach
-                
+
             </div>
             @else
             <p class="mt-6 text-center text-gray-600">Tidak ada Soal Latihan yang tersedia</p>
